310636af79396c2b066256ec8b1b8518ed67e6c9
acheron <acheron@pop-os.localdomain>
2019-12-11 19:16:14 +0100

    Base app

---
+++ src/Controller/ShoppingListController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+
+class ShoppingListController extends AbstractFOSRestController
+{
+    public function getShoppingListAction()
+    {
+        return [];
+    }
+}
